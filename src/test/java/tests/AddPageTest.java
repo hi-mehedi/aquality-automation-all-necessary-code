8964a061f6008e88eddb3e001111e5609d6607d0
Mehedi Hasan <82535547+hi-mehedi@users.noreply.github.com>
2025-08-18 02:08:15 +0600

    allure-report

--- src/test/java/tests/AddPageTest.java
+++ src/test/java/tests/AddPageTest.java
@@ -1,19 +1,27 @@
 package tests;
 
+import io.qameta.allure.Step;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.AddPage;
 import pages.HomePage;
+
+import static io.qameta.allure.Allure.step;
 
 public class AddPageTest extends BaseTest{
     HomePage homePage = new HomePage();
     AddPage addPage = new AddPage();
 
     @Test
+    @Step("Test Add Page")
     public void TestAdd(){
+        step("Click add and remove Link");
         homePage.clickaddandRemovelink();
+        step("Check add and remove page displayed");
         Assert.assertTrue(addPage.isAddPageDisplayed(), "Add Page is not displayed");
+        step("Click element Button");
         addPage.clickElementBtn();
+        step("Click Delete Button");
         addPage.clickDeleteElement();
     }
 }
--- src/test/java/tests/AlertPageTest.java
+++ src/test/java/tests/AlertPageTest.java
@@ -7,21 +7,33 @@ import org.testng.annotations.Test;
 import pages.HomePage;
 import pages.JavaScriptAlertsPage;
 
+import static io.qameta.allure.Allure.step;
+
 public class AlertPageTest extends BaseTest{
     HomePage homePage = new HomePage();
     JavaScriptAlertsPage javaScriptAlertsPage = new JavaScriptAlertsPage();
 
     @Test
     public void Test(){
+        step("Click Alert Link");
         homePage.clickAlertLink();
+        step("Check Alert page is open");
         Assert.assertTrue(javaScriptAlertsPage.isJavaScriptAlertPageDisplayed(),"Java Script Alert Page is not displayed");
+        step("Click Alert Button");
         javaScriptAlertsPage.clickAlertBtn();
+        step("Alert accept and sent text");
         AqualityServices.getBrowser().handlePromptAlert(AlertActions.ACCEPT, "Mehedi");
+        step("Refresh page");
         AqualityServices.getBrowser().getDriver().navigate().refresh();
+        step("Click second alert link");
         Assert.assertTrue(javaScriptAlertsPage.isSecondAlertBtnEnabled(), "Second Button is not Enabled");
+        step("Click second alert Button");
         javaScriptAlertsPage.clickSecondAlert();
+        step("Accept Alert");
         AqualityServices.getBrowser().handleAlert(AlertActions.ACCEPT);
+        step("Again Click second Alert Button");
         javaScriptAlertsPage.clickSecondAlert();
+        step("Click Alert Dismiss");
         AqualityServices.getBrowser().handleAlert(AlertActions.DECLINE);
     }
 }
